1d63042bc322150fa5a827c4da8426888ed5e4f6
Rakeshchary2511 <rakeshchary251199@gmail.com>
2020-07-17 11:53:42 +0530

    React-Redux for Single StateComponent git add .

---
+++ src/redux/ConfigureStore.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux';
+import {initialState, Reducer } from './Reducer';
+
+export const ConfigureStore = () => {
+    const store = createStore( 
+        Reducer,
+        initialState 
+    );
+    return store;
+}
